d12257ec0bce4cad4c6813606346c59d485e03c1
mohamedtarek1911 <mohamedtarek7670@gmail.com>
2023-04-29 19:09:08 +0200

    first deploy

--- src/hook/products/view-products-detalis-hook.js
+++ src/hook/products/view-products-detalis-hook.js
@@ -1,67 +1,69 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getOneProduct, getProductLike, getProductYouLike } from '../../redux/actions/productsAction';
-import mobile from '../../images/mobile.png'
-import { getOneCategory } from '../../redux/actions/categoryAction';
-import { getOneBrand } from '../../redux/actions/brandAction';
+import {
+  getOneProduct,
+  getProductLike,
+  getProductYouLike,
+} from "../../redux/actions/productsAction";
+import mobile from "../../images/mobile.png";
+import { getOneCategory } from "../../redux/actions/categoryAction";
+import { getOneBrand } from "../../redux/actions/brandAction";
 const ViewProductsDetalisHook = (prodID) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getOneProduct(prodID));
+  }, []);
 
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(getOneProduct(prodID))
-    }, [])
+  const oneProducts = useSelector((state) => state.allproducts.oneProduct);
+  const oneCategory = useSelector((state) => state.allCategory.oneCategory);
+  const oneBrand = useSelector((state) => state.allBrand.oneBrand);
+  const productLike = useSelector((state) => state.allproducts.productLike);
+  //to show products item
+  let item = [];
+  try {
+    if (oneProducts.data) item = oneProducts.data;
+    else item = [];
+  } catch (e) {}
 
-    const oneProducts = useSelector((state) => state.allproducts.oneProduct)
-    const oneCategory = useSelector((state) => state.allCategory.oneCategory)
-    const oneBrand = useSelector((state) => state.allBrand.oneBrand)
-    const productLike = useSelector((state) => state.allproducts.productLike)
-    //to show products item
-    let item = [];
-    if (oneProducts.data)
-        item = oneProducts.data;
-    else
-        item = []
+  useEffect(() => {
+    if (item.category) dispatch(getOneCategory(item.category));
+    if (item.brand) dispatch(getOneBrand(item.brand));
+    if (item.category) dispatch(getProductLike(item.category));
+  }, [item]);
 
-    useEffect(() => {
-        if (item.category)
-            dispatch(getOneCategory(item.category))
-        if (item.brand)
-            dispatch(getOneBrand(item.brand))
-        if (item.category)
-            dispatch(getProductLike(item.category))
-
-    }, [item])
-
-
-    //to view images gallery
-    let images = []
+  //to view images gallery
+  let images = [];
+  try {
     if (item.images)
-        images = item.images.map((img) => { return { original: img } })
+      images = item.images.map((img) => {
+        return { original: img };
+      });
     else {
-        images = [{ original: `${mobile}` }]
+      images = [{ original: `${mobile}` }];
     }
+  } catch (e) {}
 
+  //to show category item
+  let cat = [];
+  try {
+    if (oneCategory.data) cat = oneCategory.data;
+    else cat = [];
+  } catch (e) {}
 
-    //to show category item
-    let cat = [];
-    if (oneCategory.data)
-        cat = oneCategory.data;
-    else
-        cat = []
+  //to show brand item
+  let brand = [];
+  try {
+    if (oneBrand.data) brand = oneBrand.data;
+    else brand = [];
+  } catch (e) {}
 
-    //to show brand item
-    let brand = [];
-    if (oneBrand.data)
-        brand = oneBrand.data;
-    else
-        brand = []
+  let prod = [];
+  try {
+    if (productLike) prod = productLike.data;
+    else prod = [];
+  } catch (e) {}
 
-    let prod = []
-    if (productLike)
-        prod = productLike.data;
-    else
-        prod = []
-    return [item, images, cat, brand, prod]
-}
+  return [item, images, cat, brand, prod];
+};
 
-export default ViewProductsDetalisHook
+export default ViewProductsDetalisHook;
